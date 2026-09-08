42e23476372f32d0eeeba2c9e41a9985d0ab13ae
muradvcv <muradvcv@gmail.com>
2026-09-08 07:35:15 +0600

    some error fixed

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,1 +1,10 @@
-
+function matchCharacter(str,char){
+  let count =0
+  for(let c of str){
+    if(c==char){
+      count ++;
+    }
+  }
+  return count;
+}
+console.log(matchCharacter('ddd','h'));
